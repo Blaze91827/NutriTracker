7740f71463a6216c25c5fe37600275021400a26f
Blaze91827 <58652344+Blaze91827@users.noreply.github.com>
2021-05-09 17:57:31 -0400

    Add files via upload

---
+++ NutriTracker/Models/Food.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace NutriTracker.Models
+{
+    class Food
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string name { get; set; }
+        public int calories { get; set; }
+        public int fats { get; set; }
+        public int carbs { get; set;  }
+        public int proteins { get; set; }
+    }
+}
---
+++ NutriTracker/Views/MainView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using NutriTracker.ViewModels;
+
+namespace NutriTracker.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainView : ContentPage
+    {
+        public int totalCalories { get; set; }
+        public int totalFats{ get; set; }
+        public int totalCarbs { get; set; }
+        public int totalProteins { get; set; }
+        public MainView(MainViewModel viewModel)
+        {
+            InitializeComponent();
+            viewModel.Navigation = Navigation;
+            BindingContext = viewModel;
+
+            ItemsListView.ItemSelected += (s, e) =>
+            ItemsListView.SelectedItem = null;
+            
+        }
+    }
+}
