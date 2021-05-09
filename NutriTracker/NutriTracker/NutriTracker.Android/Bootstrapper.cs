7b9c77969842e34109efc7a7c02ab191c9206e15
Blaze91827 <58652344+Blaze91827@users.noreply.github.com>
2021-05-09 17:11:33 -0400

    Add files via upload

---
+++ NutriTracker/NutriTracker/NutriTracker.Android/Bootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using System.Linq;
+using System.Reflection;
+using Xamarin.Forms;
+using NutriTracker.Views;
+using NutriTracker.Repositories;
+using NutriTracker.ViewModels;
+
+namespace NutriTracker.Android
+{
+    public class Bootstrapper : NutriTracker.Bootstrapper
+    {
+        public static void Init()
+        {
+            var instance = new Bootstrapper();
+        }
+    }
+}
